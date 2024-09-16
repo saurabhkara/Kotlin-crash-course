d9234ace1c177408caece2ebdfbc7d4fc322230d
Saurabh <saurabkumar679@gmail..com>
2024-09-16 08:44:02 +0530

    variables

---
+++ src/Basics.kt
@@ -0,0 +1,11 @@
+// var is mutuable and val is immutable
+const val name= "Saurabh kumar"
+fun main (){
+    var a=10;
+    val b="Saurabh"
+    println(a);
+    a=15;
+    println(a);
+    println(b)
+    println(name)
+}
