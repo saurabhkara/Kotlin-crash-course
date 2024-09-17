71076772c4c0278dfa713aa105469c021fca5791
Saurabh <saurabkumar679@gmail..com>
2024-09-17 07:24:18 +0530

    data types

--- src/Basics.kt
+++ src/Basics.kt
@@ -1,5 +1,6 @@
 // var is mutuable and val is immutable
 const val name= "Saurabh kumar"
+const val PI :Double= 3.14
 fun main (){
     var a=10;
     val b="Saurabh"
@@ -8,4 +9,12 @@ fun main (){
     println(a);
     println(b)
     println(name)
+    println(PI)
+    var ab:Int=10;
+    var ac:Double=45.5;
+    var ad:Float=45.865F;
+    var ae:Boolean=false;
+    var af:Long=10000L
+    var stringLiteral:String = "Learning Kotlin is fun";
+    println("String Literals : $stringLiteral");
 }
