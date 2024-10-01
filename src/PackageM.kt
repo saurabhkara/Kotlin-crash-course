d3efef7800f0c72a108b0fabf55ebd6141fa57e6
Saurabh <saurabkumar679@gmail..com>
2024-10-01 08:40:33 +0530

    package

---
+++ src/PackageM.kt
@@ -0,0 +1,8 @@
+package src;
+
+
+fun main(){
+    greet("Saurabh")
+}
+
+// You need to run both files at same time
---
+++ src/greet.kt
@@ -0,0 +1,5 @@
+ package src
+ 
+ fun greet(name:String){
+    println("Hello $name");
+ }
